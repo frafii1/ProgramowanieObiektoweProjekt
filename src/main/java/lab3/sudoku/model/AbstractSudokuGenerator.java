cbbf6abda0a67cf594ea58ee7262dfb8a4bc0e7a
frafii1 <filipfrackowiak12@gmail.com>
2026-01-23 20:03:38 +0100

    Create AbstractSudokuGenerator.java

---
+++ src/main/java/lab3/sudoku/model/AbstractSudokuGenerator.java
@@ -0,0 +1,11 @@
+package lab3.sudoku.model;
+
+public abstract class AbstractSudokuGenerator {
+
+    protected abstract SudokuBoard generateSolvedBoard();
+
+    public SudokuBoard generate(int filledFields) {
+        SudokuBoard solved = generateSolvedBoard();
+        return solved; //  rozwiązanie
+    }
+}

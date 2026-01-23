99bca33106a943f1a2e03ecbb9a28d9ffa02201b
frafii1 <filipfrackowiak12@gmail.com>
2026-01-23 17:23:11 +0100

    Update SudokuBoard.java

--- src/main/java/lab3/sudoku/model/SudokuBoard.java
+++ src/main/java/lab3/sudoku/model/SudokuBoard.java
@@ -5,6 +5,16 @@ package lab3.sudoku.model;
  * @author Ążej
  * reprezentacja całej planszy 9x9 wartości tworzy generator planszy
  * przechowuje wszystkie 81 pól typu SudokuSingleField
+ * Klasa reprezentująca całą planszę gry Sudoku.
+ * <p>
+ * Pełni rolę kontenera dla 81 pól typu {@link SudokuSingleField}.
+ * Odpowiada za:
+ * <ul>
+ * <li>Przechowywanie struktury siatki 9x9.</li>
+ * <li>Inicjalizację pustych pól przy tworzeniu obiektu.</li>
+ * <li>Udostępnianie metod dostępowych do poszczególnych komórek planszy.</li>
+ * </ul>
+ *
  */
 public class SudokuBoard {
     public static final int SIZE = 9;
@@ -22,10 +32,18 @@ public class SudokuBoard {
     public SudokuSingleField getField(int x, int y) {
         return board[x][y];
     }
-    
+    /**
+     * Ustawia wartość liczbową w wybranym polu i blokuje jego edycję.
+     * <p>
+     * Metoda ta jest przeznaczona głównie do inicjalizacji planszy (ustawiania
+     * wartości początkowych), ponieważ wywołuje {@code setEditable(false)}.
+     *
+     * @param x Indeks wiersza (0-8).
+     * @param y Indeks kolumny (0-8).
+     * @param value Wartość do wpisania w pole (1-9).
+     */
     public void setFieldValue(int x, int y, int value) {
         board[x][y].setValue(value);
         board[x][y].setEditable(false); 
     }
 }
-
